99b39bd8070ae78e5d53330e8fd2d256882c2123
Shay Halimi <shay.halimi@plus500.com>
2023-04-23 18:54:40 +0300

    .

--- GptJunior/GptJunior.cs
+++ GptJunior/GptJunior.cs
@@ -9,18 +9,50 @@ public class GptJunior : IGptJunior
 {
     private readonly IProjectManager _projectManager;
     private readonly IGptDeveloper _gptDeveloper;
+    private readonly IGitManager _gitManager;
 
-    public GptJunior(IProjectManager projectManager, IGptDeveloper gptDeveloper)
+    public GptJunior(
+        IProjectManager projectManager, 
+        IGptDeveloper gptDeveloper, 
+        IGitManager gitManager)
     {
         _projectManager = projectManager;
         _gptDeveloper = gptDeveloper;
+        _gitManager = gitManager;
     }
 
     public async Task Create(string description)
     {
-        var programManager = _projectManager.GetProgram();
-        var functionName = await Develop(description, programManager);
+        var functionName = await Develop(description);
+        await Fix(functionName);
+    }
 
+    private async Task<string> Develop(string description)
+    {
+        var programManager = _projectManager.GetProgram();
+        
+        var response = await _gptDeveloper.Develop(description);
+        
+        string functionName = response.FunctionName[0];
+        List<string> functionImplementation = response.FunctionImplementation;
+        List<string> flow = response.Flow;
+        var expectedResult = response.ExpectedResult;
+
+        var classManager = _projectManager.GetClass(functionName);
+        classManager.AddFunction(functionImplementation);
+        var testFunction = Helpers.CreateFunctionWrapper("Test");
+        flow = flow.Select(line => line = "   " + line).ToList();
+        testFunction.InsertRange(2, flow);
+        classManager.AddFunction(testFunction);
+        
+        programManager.AddFlow(new List<string> {$"var test = new {functionName}Class();"});
+        programManager.Save();
+
+        return functionName;
+    }
+
+    private async Task Fix(string functionName)
+    {
         // Todo: Remember past iterations
         for (var i = 0; i < 10; i++)
         {
@@ -44,28 +76,6 @@ public class GptJunior : IGptJunior
             }
         }
     }
-
-    private async Task<string> Develop(string description, IProgramManager programManager)
-    {
-        var response = await _gptDeveloper.Develop(description);
-        
-        string functionName = response.FunctionName[0];
-        List<string> functionImplementation = response.FunctionImplementation;
-        List<string> flow = response.Flow;
-        var expectedResult = response.ExpectedResult;
-
-        var classManager = _projectManager.GetClass(functionName);
-        classManager.AddFunction(functionImplementation);
-        var testFunction = Helpers.CreateFunctionWrapper("Test");
-        flow = flow.Select(line => line = "   " + line).ToList();
-        testFunction.InsertRange(2, flow);
-        classManager.AddFunction(testFunction);
-        
-        programManager.AddFlow(new List<string> {$"var test = new {functionName}Class();"});
-        programManager.Save();
-
-        return functionName;
-    }
 }
 
 public static class GptJuniorsFactory
@@ -74,8 +84,12 @@ public static class GptJuniorsFactory
     {
         var projectManager = ProjectManagersFactory.CreateProjectManager();
         var gptDeveloper = GptDevelopersFactory.CreateGptDeveloper();
+        var gitManager = GitManagersFactory.CreateGitManager();
 
-        var gptJunior = new GptJunior(projectManager, gptDeveloper);
+        var gptJunior = new GptJunior(
+            projectManager, 
+            gptDeveloper,
+            gitManager);
 
         return gptJunior;
     }
