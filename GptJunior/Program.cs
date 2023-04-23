0cb8a83e4a8b0b7de501c361d83770c6b60640a0
Shay Halimi <shay.halimi@plus500.com>
2023-04-23 18:12:46 +0300

    .

--- GptJunior/Program.cs
+++ GptJunior/Program.cs
@@ -1,20 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using GptJunior;
-using OpenAI;
-using OpenAI.Chat;
-using OpenAI.Models;
 
 var projectManager = ProjectManagersFactory.CreateProjectManager();
 var gptDeveloper = GptDevelopersFactory.CreateGptDeveloper();
 
 var gptJunior = new GptJunior.GptJunior(projectManager, gptDeveloper);
-await gptJunior.Create("a function that add four to a number");
+await gptJunior.Create("a function that take a number and return the closest odd number that is higher than it");
 
-// var wow = GptJuniorsFactory.CreateGptJunior();
-// wow.Create("a function that add four to a number");
-
-
-// var second = GptDevelopersFactory.CreateGptDeveloper();
-// var d = await second.Develop("a function that add four to a number");
-// Console.WriteLine(d.FunctionName[0]);
